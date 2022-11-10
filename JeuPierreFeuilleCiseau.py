e6ebea77a86cac290bf9d883cd5cac0418eea27e
Lilian <lviennet@gaming.tech>
2022-11-10 12:18:13 +0100

    Code mis à jour

--- JeuPierreFeuilleCiseau.py
+++ JeuPierreFeuilleCiseau.py
@@ -1,22 +1,74 @@
+#DEBUT
+#Tant que 
+    #On apporte la bibliothèque "random"  pour utiliser des choix aléatoires 
+    #Création d’une liste d’options "choices" jouable 
+    #L’ordi joue aléatoirement  dans la liste "choice", compris entre 0 et 2
+    #le joueur rentre une des trois données possible : Pierre, Feuille, Ciseau
+    #tant que le joueur ne perd pas
+        #Si le choix du joueur est égale à celui de l'ordi
+            #Afficher "Égalité"
+        #Sinon le joueur est égale à Pierre
+            #Si l'ordi est égale à Papier
+                #Afficher "Vous avez perdu…"  l'ordi "à recouvert" le joueur
+            #Sinon
+                #Afficher "Vous avez gagné !" le joueur "à écrasé" l'ordi
+        #Sinon le joueur est égale à Papier
+            #Si l'ordi est égale à Ciseau
+                #Afficher "Vous avez perdu…"  l'ordi "à coupé" le joueur
+            #Sinon
+                #Afficher "Vous avez gagné !" le joueur "à recouvert" l'ordi
+        #Sinon le joueur est égale à Ciseau
+            #Si l'ordi est égale à Pierre
+                #Afficher "Vous avez perdu…"  l'ordi "à écrasé" le joueur
+            #Sinon
+                #Afficher "Vous avez gagné !" le joueur "à coupé" l'ordi
+            #Joueur égale faux
+            #L’ordi joue aléatoirement dans la liste "choice", compris entre 0 et 2
+#Sinon
+    #Afficher "A la prochaine !"
+#FIN
+
+
 import random
 
-COUP = ("Pierre", "Feuille", "Ciseaux")
-#Demande au joueur si il veut jouer
-while input("Voulez vous jouez ? (oui/non): ").lower() != "non":
-    #Titre du jeu
-    print("\n------------------------------------")
-    print("Le jeu du: Pierre - Feuille - Ciseaux")
-    print("------------------------------------\n")
-    #Demander au joueur de choisir un chiffre correspondan
-    a = int(input("Choisissez un chiffre:\n0: Pierre\n1: Feuille\n2: Ciseaux\n-> "))
-    b = random.choice(range(3))
- 
-    print("\n{} VS {}".format(COUP[a], COUP[b]))
-    if a == b:
-        print("ÉGALITÉ\n")
-    elif (a>b and b+1==a) or (a<b and a+b==2):
-        print("VOUS GAGNEZ\n")
-    else:
-        print("VOUS PERDEZ\n")
+while input("On lance une partie ? (oui/non): ").lower() != "n":
+
+    #Création d’une liste d’options jouable
+    choices = ["Pierre", "Papier", "Ciseau"]
+
+    #L’ordi joue aléatoirement
+    computer = choices[random(0,2)]
+    #le joueur rentre une des trois données possible : Pierre, Feuille, Ciseau
+    player = input("Pierre, Papier, Ciseau?")
+
+    while player == False:
+
+        if player == computer:
+            print("Égalité !")
+        elif player == "Pierre":
+            if computer == "Papier":
+                print("Vous avez perdu…", computer,  "à recouvert", player)
+            else:
+                print("Vous avez gagné !", player,  "à écrasé", computer)
+        elif player == "Papier":
+            if computer == "Ciseau":
+                print("Vous avez perdu…", computer, "à coupé", player)
+            else:
+                print("Vous avez gagné !", player,  "à recouvert", computer)
+
+        elif player == "Ciseau":
+            if computer == "Pierre":
+                print("Vous avez perdu…", computer, "à écrasé", player)
+            else:
+                print("Vous avez gagné !", player, "à coupé", computer)
+        else:
+            print("Votre orthographe est incorrecte, vérifiez et réessayez !")
+
+        player = False
+
+        computer = choices[random(0,2)]
 else:
-    print("Bye Bye")
+    print("A la prochaine !")
+
+
+#Definir une fonction...
