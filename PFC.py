a6416421481d4fbace05191eae0a0bad5df6e7f2
Lilian <lviennet@gaming.tech>
2022-11-15 09:29:35 +0100

    Correction du PFC et fin du morpion

--- PFC.py
+++ PFC.py
@@ -1,41 +1,54 @@
-from random import choices
-from random import randint
- 
-while input("On lance une partie ? (oui/non): ").lower() != "non":
- 
-    #Création d’une liste d’options jouable
-    choices = ["Pierre", "Papier", "Ciseaux"]
- 
-    #L’ordi joue aléatoirement
-    computer = choices[randint(0,2)]
- 
-    while player == False:
-        #le joueur rentre une des trois données possible : Pierre, Feuille, Ciseau
-        player = input("Que voulez vous jouer?\n")
-        if player == computer:
-            print("Égalité !")
-        elif player == "Pierre":
-            if computer == "Papier":
-                print("Vous avez perdu…", computer,  "à recouvert", player)
-            else:
-                print("Vous avez gagné !", player,  "à écrasé", computer)
-        elif player == "Papier":
-            if computer == "Ciseaux":
-                print("Vous avez perdu…", computer, "à coupé", player)
-            else:
-                print("Vous avez gagné !", player,  "à recouvert", computer)
- 
-        elif player == "Ciseaux":
-            if computer == "Pierre":
-                print("Vous avez perdu…", computer, "à écrasé", player)
-            else:
-                print("Vous avez gagné !", player, "à coupé", computer)
+import random
+
+jeu = ['pierre', 'feuille', 'ciseaux', 'puit']
+print("Bienvenue dans le jeu Pierre Feuille Ciseaux !")
+
+def play():
+
+    bot = random.choice(jeu)
+
+    choix = input('Choisissez entre "pierre", "feuille", "puit ou "ciseaux" !    ')
+    if(choix == bot):
+        print('Egalité ! Le robot a choisit', bot, 'comme vous !')
+    else:
+        if(choix == 'pierre' and bot == 'feuille'):
+            print('Vous avez gagner ! La pierre écrase la', bot, '!')
+        elif(choix == 'feuille' and bot == 'pierre'):
+            print('Vous avez perdu ! La feuille se fait écraser par la', bot)
+        elif(choix == 'ciseaux' and bot == 'pierre'):
+            print('Vous avez perdu ! La pierre écrase le ciseaux !')
+        elif(choix == 'pierre' and bot == 'ciseaux'):
+            print('Vous avez gagner ! La pierre écrase le ciseaux !')
+        elif(choix == 'ciseaux' and bot == 'feuille'):
+            print('Vous avez gagner ! Le ciseaux coupe la feuille !')
+        elif(choix == 'feuille' and bot == 'ciseaux'):
+            print('Vous avez perdu ! Le ciseaux coupe la feuille !')
+        elif(choix == 'pierre' and bot == 'puit'):
+            print('Vous avez perdu ! La pierre tombe dans le puit !')
+        elif(choix == 'puit' and bot == 'pierre'):
+            print('Vous avez gagner ! La pierre tombe dans le puit !')
+        elif(choix == 'feuille' and bot == 'puit'):
+            print('Vous avez gagner ! La feuille bouche le puit !')
+        elif(choix == 'puit' and bot == 'feuille'):
+            print('Vous avez perdu ! Le puit se fait boucher par la feuille !')
+        elif(choix == 'ciseaux' and bot == 'puit'):
+            print('Vous avez perdu ! Le puit écrase le ciseaux !')
+        elif(choix == 'puit' and bot == 'ciseaux'):
+            print('Vous avez gagner ! Le puit écrase le ciseaux !')
         else:
-            print("Votre orthographe est incorrecte, vérifiez et réessayez !")
- 
-        player = False
- 
-        computer = choices[randint(0,2)]
- 
-else:
-    print("A la prochaine !")
+            print('Une erreur est survenue !')
+
+    rejouer()
+        
+def rejouer():
+    choix2 = input('Voulez vous rejouer ? (Y/N)')
+    if(choix2 == 'Y'):
+        play()
+    if(choix2 == 'y'):
+        play()
+    if(choix2 == 'yes'):
+        play()
+    else:
+        exit
+
+play()
